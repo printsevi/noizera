625414c4a793c2cf5ac9ade07d4db6ac627075dc
printsev <printsev.ildar@gmail.com>
2024-11-25 00:08:54 +0100

    35

--- reactapp/app/collections/[collectionPublicId]/page.tsx
+++ reactapp/app/collections/[collectionPublicId]/page.tsx
@@ -1,9 +1,10 @@
 import MusicCollectionContent from './components/MusicCollectionContent';
 
 const MusicCollection = async ({ params }: { params: { collectionPublicId: string } }) => {
+  const { collectionPublicId } = await params;
   return (
     <div className='my-0 mx-auto max-w-5xl h-full'>
-      <MusicCollectionContent collectionPublicId={params.collectionPublicId} />
+      <MusicCollectionContent collectionPublicId={collectionPublicId} />
     </div>
   );
 };
