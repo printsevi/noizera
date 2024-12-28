8b815d90d3d2eeb127c7fb944e1de70a89fe5860
printsev <printsev.ildar@gmail.com>
2024-12-28 12:34:48 +0100

    173

--- reactapp/components/ImageUploader.tsx
+++ reactapp/components/ImageUploader.tsx
@@ -116,7 +116,6 @@ const ImageUploader: React.FC<ImageUploadProps> = ({ uploadedImageUrl, onUpload,
               src={uploadedImageUrl}
               alt="Uploaded cover image"
               fill
-              className="object-cover"
             />
             <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity">
               <Button
