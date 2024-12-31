20ba87c11b475c5cc7b923a30e4245686dc1d601
printsev <printsev.ildar@gmail.com>
2025-01-01 00:11:33 +0100

    187

--- reactapp/providers/SongProvider.tsx
+++ reactapp/providers/SongProvider.tsx
@@ -113,7 +113,7 @@ const SongContextProvider = ({ children }: Props) => {
 
     const prev = () => {
         const currentIndex = songs.findIndex(x => x.song.songPublicId.toUpperCase() === currentSong?.song.songPublicId.toUpperCase());
-        if (currentIndex === -1 || (currentIndex === 0 && songs.length === 1)) {
+        if (currentIndex === -1 || currentIndex === 0) {
             return;
         }
         setCurrentSong(songs[currentIndex - 1]);
