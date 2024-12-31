ecbddf7124c0a560143160bf162e244a780b34f1
printsev <printsev.ildar@gmail.com>
2024-12-31 22:02:40 +0100

    184

--- reactapp/providers/SongProvider.tsx
+++ reactapp/providers/SongProvider.tsx
@@ -100,7 +100,7 @@ const SongContextProvider = ({ children }: Props) => {
 
     const next = () => {
         const currentIndex = songs.findIndex(x => x.song.songPublicId.toUpperCase() === currentSong?.song.songPublicId.toUpperCase());
-        if (currentIndex <= 0) {
+        if (currentIndex === -1 || (currentIndex === 0 && songs.length === 1)) {
             //get next bunch of recommended songs
             return;
         }
@@ -113,7 +113,7 @@ const SongContextProvider = ({ children }: Props) => {
 
     const prev = () => {
         const currentIndex = songs.findIndex(x => x.song.songPublicId.toUpperCase() === currentSong?.song.songPublicId.toUpperCase());
-        if (currentIndex <= 0) {
+        if (currentIndex === -1 || (currentIndex === 0 && songs.length === 1)) {
             return;
         }
         setCurrentSong(songs[currentIndex - 1]);
