28d53844f3b36b1f677d32295929ca14f545c135
printsev <printsev.ildar@gmail.com>
2025-01-10 17:52:21 +0100

    delete the validation

--- api/src/Noizera.Application/CQRS/Auth/Register/RegisterValidator.cs
+++ api/src/Noizera.Application/CQRS/Auth/Register/RegisterValidator.cs
@@ -14,7 +14,7 @@ public sealed class RegisterValidator : AbstractValidator<RegisterCommand>
             .Matches("[A-Z]").WithMessage("Password must contain one or more capital letters.")
             .Matches("[a-z]").WithMessage("Password must contain one or more lowercase letters.")
             .Matches(@"\d").WithMessage("Password must contain one or more digits.")
-            .Matches(@"[~`¿¡!#$%\^&*€£@+÷=\-\[\]\\';,/{}\(\)|\\"":<>\?\.\_]").WithMessage("Password must contain one or more special characters.")
+            //.Matches(@"[~`¿¡!#$%\^&*€£@+÷=\-\[\]\\';,/{}\(\)|\\"":<>\?\.\_]").WithMessage("Password must contain one or more special characters.")
             //.Matches("^[^£# “”]*$").WithMessage("'{PropertyName}' must not contain the following characters £ # “” or spaces.")
             .WithMessage("Password is not strong enough");
 
