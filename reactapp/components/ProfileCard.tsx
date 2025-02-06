454e76f1876e6a6325e40673a6b6c06939846aa6
printsev <printsev.ildar@gmail.com>
2025-02-06 20:32:42 +0100

    FE: remove context from profile cards

--- reactapp/components/ProfileCard.tsx
+++ reactapp/components/ProfileCard.tsx
@@ -55,96 +55,27 @@ export function ProfileCard({
 
   return (
     <div>
-      <ContextMenu>
-        <ContextMenuTrigger>
-          <div className="overflow-hidden rounded-full cursor-pointer">
-            <Card className="border-none">
-              <CardContent className="flex aspect-square items-center justify-center group relative">
-                <ImageWithFallback
-                  src={getProfileImageSrc(publicId)}
-                  sizes="500"
-                  priority={false}
-                  fill
-                  alt={name}
-                  fallbackSrc="/images/user.png"
-                  className={cn(
-                    "object-cover transition-all group-hover:scale-105 rounded-full"
-                  )}
-                />
-                <div onClick={() => router.push(`/profiles/${username.toLowerCase()}`)} className="absolute bg-black rounded-md bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-end group-hover:opacity-100 transition flex-col justify-between p-2.5">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <button className="hover:scale-125 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
-                        <EllipsisVerticalIcon size={25} />
-                      </button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                      <DropdownMenuGroup>
-                        {/* {isAuthenticated && <DropdownMenuItem
-                          onClick={(e: { stopPropagation: () => void; }) => {
-                            e.stopPropagation();
-                            onSaveDeleteToggle();
-                          }}>
-                          {collectionIsSaved ? <CopyMinus className="mr-2 h-4 w-4" /> : <CopyPlus className="mr-2 h-4 w-4" />}
-                          <span>{collectionIsSaved ? "Remove from library" : "Save to library"}</span>
-                        </DropdownMenuItem>}
-                        <DropdownMenuItem onClick={(e: { stopPropagation: () => void; }) => {
-                          e.stopPropagation();
-                          copyCollectionUrl();
-                        }}>
-                          <Forward className="mr-2 h-4 w-4" />
-                          <span>Share</span>
-                        </DropdownMenuItem> */}
-                      </DropdownMenuGroup>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                  {/* <button
-                    onClick={async (e) => {
-                      e.stopPropagation();
-                      await fetchAndPlay(publicId);
-                    }}
-                    className="hover:scale-150 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition"
-                  >
-                    <PlayIcon size={35} />
-                  </button> */}
-                </div>
-              </CardContent>
-            </Card>
-          </div>
-        </ContextMenuTrigger>
-        <ContextMenuContent className="w-40">
-          {/* {isAuthenticated && <ContextMenuItem
-            onClick={(e: { stopPropagation: () => void; }) => {
-              e.stopPropagation();
-              onSaveDeleteToggle();
-            }}>
-            {collectionIsSaved ? <CopyMinus className="mr-2 h-4 w-4" /> : <CopyPlus className="mr-2 h-4 w-4" />}
-            <span>{collectionIsSaved ? "Remove from library" : "Save to library"}</span>
-          </ContextMenuItem>}
-          <ContextMenuItem onClick={(e: { stopPropagation: () => void; }) => {
-            e.stopPropagation();
-            copyCollectionUrl();
-          }}>
-            <Forward className="mr-2 h-4 w-4" />
-            <span>Share</span>
-          </ContextMenuItem> */}
-        </ContextMenuContent>
-      </ContextMenu>
+      <div className="overflow-hidden rounded-full cursor-pointer">
+        <Card className="border-none">
+          <CardContent className="flex aspect-square items-center justify-center group relative">
+            <ImageWithFallback
+              src={getProfileImageSrc(publicId)}
+              sizes="500"
+              priority={false}
+              fill
+              alt={name}
+              fallbackSrc="/images/user.png"
+              className={cn(
+                "object-cover transition-all group-hover:scale-105 rounded-full"
+              )}
+            />
+            <div onClick={() => router.push(`/profiles/${username.toLowerCase()}`)} className="absolute bg-black rounded-md bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-end group-hover:opacity-100 transition flex-col justify-between p-2.5">
+            </div>
+          </CardContent>
+        </Card>
+      </div>
       <div className="text-sm text-center">
         <h3 className="mt-2 font-medium truncate"><Link href={`/profiles/${username.toLowerCase()}`} key={`/profiles/${username}`} className="hover:underline">{name}</Link></h3>
-        {/* <p className="text-muted-foreground line-clamp-2 text-ellipsis">{songCount === 1 ? <span>Single</span> : <span>{songCount}&nbsp;songs</span>}&nbsp;•&nbsp;{ownerProfileType === ProfileType.Artist && <React.Fragment>
-          <Link href={`/profiles/${ownerUsername.toLowerCase()}`} className="hover:underline truncate max-w-full">
-            {ownerName}
-          </Link>
-          {credits.length > 0 && <span>&nbsp;•&nbsp;</span>}
-        </React.Fragment>}
-          {credits.map((credit, index) => (
-            <React.Fragment key={credit.username}>
-              {index > 0 && <span>&nbsp;•&nbsp;</span>}
-              {credit.username ? <Link href={`/profiles/${credit.username.toLowerCase()}`} className="hover:underline truncate max-w-full">{credit.name}</Link> : <span className="truncate max-w-full">{credit.profileName}</span>}
-            </React.Fragment>
-          ))}</p>
-        {ownerProfileType === ProfileType.Label && <p className="text-sm text-muted-foreground line-clamp-2 text-ellipsis">by&nbsp;<Link href={`/profiles/${ownerUsername.toLowerCase()}`} key={`/profiles/${ownerUsername.toLowerCase()}`} className="hover:underline">{ownerName}</Link></p>} */}
       </div>
     </div>
   )
